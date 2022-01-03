560b7911ecc5e90f5bc1dc63a50352cc8c52ddae
Kalkivi Khunt <calkikhunt123@gmail.com>
2022-01-03 22:26:33 +0530

    add app

---
+++ vastmed_report_customization/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Vastmed Report Customization",
+    "version": "14.0.0.1",
+    "category": "Extra",
+    "description": "report customization for vastmed",
+    "author": "Kalkivi Khunt",
+    "website": "",
+    "depends": [
+        "account",
+        "stock"
+    ],
+    "data": [
+        "data/vastmed_report_paperformat.xml",
+        "report/vastmed_report_layout.xml",
+        "report/vastmed_report_invoice.xml",
+        "report/vastmed_report_saleorder.xml",
+        "report/vastmed_report_deliveryslip.xml",
+    ],
+    "qweb": [],
+
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
