e9a62025b4a3ebfe6e4ca834fcd538389fd5f494
malasad12 <80427033+malasad12@users.noreply.github.com>
2022-02-12 18:48:12 -0600

    [ADD] app mapsly_connector

---
+++ mapsly_connector/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Map for analysis, routing & territories",
+
+    'summary': "Connect your data to Mapsly",
+    'description': """
+        Connects your models and records to Mapsly
+    """,
+    'author': "Mapsly",
+    'website': "https://mapsly.com/",
+    'license': "OPL-1",
+
+    'version': '14.0.0.1.0',
+    'category': 'Connector',
+    'depends': ['connector', 'base'],
+    'data': [
+        "views/assets.xml",
+        "views/data.xml"
+    ],
+    'images': [
+        'static/description/thumbnail.png',
+        'static/description/desc_img_02.png',
+        'static/description/desc_img_03.png',
+        'static/description/desc_img_04.jpeg',
+        'static/description/desc_img_05.png',
+        'static/description/desc_img_06.jpeg',
+        'static/description/desc_img_07.png',
+        'static/description/desc_img_08.png',
+        'static/description/icon.png'
+    ],
+    'qweb': [],
+    'installable': True
+}
